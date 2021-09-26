4b39d2fc87709f42fc32cd0e6203249ad1a38821
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2021-09-25 20:49:25 -0700

    git revert 7d18e87

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
-import Layout from "../components/layout";
-import Banner from "../sections/banner";
-import SEO from "../components/seo";
+import {Link} from "gatsby";
+import {Helmet} from "react-helmet";
 
 const IndexPage = () => {
     return (
-        <Layout>
-            <SEO title="PyK-Ar | Potassium-Argon dating software" />
-            <Banner />
-        </Layout>
+        <main>
+            <Helmet>
+                <title>PyK-Ar | Potassium-Argon Dating CICESE</title>
+                <meta name="description" content="Potassium-Argon Dating (K-Ar) CICESE | Carlos Eduardo Sanchez Torres" />
+            </Helmet>
+            <p>Landing page</p>
+            <Link to="/admin/">Admin</Link>
+        </main>
     )
 };
 
