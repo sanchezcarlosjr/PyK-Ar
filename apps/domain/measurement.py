44b56c5721bb787ffdfffd00aabf269c3c5fdad1
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2021-10-20 08:34:33 -0700

    refactor(apps measurement): type method

--- apps/domain/measurement.py
+++ apps/domain/measurement.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-from domain.experiment import Experiment
+from domain.experiment import Experiment, Cycle
 
 
 @dataclass
@@ -16,5 +16,5 @@ class Measurement:
     updatedby: str = ""
     createdby: str = ""
 
-    def filter_corrected_cycles(self):
+    def filter_corrected_cycles(self) -> list[Cycle]:
         return [cycle for experiment in self.experiments for cycle in experiment.cycles if cycle.is_corrected()]
