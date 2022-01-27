ee1475d4b5234609eaebb7603a2cb18fc2d667da
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-01-26 19:50:08 -0800

    feat(ui):  show columns

--- web/src/app/potassium-argon-age-calculation/PotassiumArgonAgeCalculationList.tsx
+++ web/src/app/potassium-argon-age-calculation/PotassiumArgonAgeCalculationList.tsx
@@ -12,8 +12,10 @@ export const PotassiumArgonAgeCalculationList = (props: any) => (
         <Datagrid rowClick="edit">
             <TextField source="age" label="Age x 10^6 (Million years ago)"/>
             <TextField source="spectrum_user_name" label="Spectrum user name" />
-            <TextField source="delta" label="Delta"/>
             <TextField source="analysis_date" label="Analysis Date"/>
+            <TextField source="weight" label="Weight (grams)"/>
+            <TextField source="delta" label="Delta"/>
+            <TextField source="gramsOfK" label="K (grams)"/>
             <TextField source="dalrymple_error" label="+- Dalrymple Error (Million years ago)"/>
             <DeleteButton label=""/>
         </Datagrid>
