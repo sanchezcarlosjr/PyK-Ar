b5ff41088e451198f2f078b2fa9a40d479051c78
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-01-27 15:51:19 -0800

    feat(ui): show new fields

--- web/src/app/potassium-argon-age-calculation/PotassiumArgonCalculationsEdit.tsx
+++ web/src/app/potassium-argon-age-calculation/PotassiumArgonCalculationsEdit.tsx
@@ -1,13 +1,34 @@
-import {Edit, SimpleForm, TextInput, DateInput} from "react-admin";
+import {DateInput, Edit, NumberInput, SimpleForm, TextInput} from "react-admin";
 import * as React from "react";
 
 export const PotassiumArgonCalculationsEdit = (props: any) => (
     <Edit {...props}>
         <SimpleForm>
-            <DateInput source="createdate" disabled label="Created date" />
-            <TextInput source="age"/>
-            <TextInput source="york_fit_error"/>
-            <TextInput source="dalrymple_error"/>
+            <TextInput source="id"/>
+            <DateInput source="analysis_date" label="Analysis date"/>
+            <TextInput source="spectrum_user_name"/>
+            <NumberInput source="age" disabled/>
+            <NumberInput source="age_standard_deviation" disabled/>
+            <NumberInput source="ar36_divides_ar38_t" disabled/>
+            <NumberInput source="Ar40_Ar38_ratio" disabled/>
+            <NumberInput source="Ar38_Ar36_ratio" disabled/>
+            <NumberInput source="Ar40_Ar38_ratios_in_the_gas_mixture" disabled/>
+            <NumberInput source="Ar38_Ar36_ratios_in_the_gas_mixture" disabled/>
+            <NumberInput source="Ar40" disabled/>
+            <NumberInput source="Ar38" disabled/>
+            <NumberInput source="Ar36" disabled/>
+            <NumberInput source="ar40_divides_ar38_t" disabled/>
+            <NumberInput source="moles_Ar38_in_tracer" disabled/>
+            <NumberInput source="percentage_of_Ar40_rad_in_the_analysis" disabled/>
+            <NumberInput source="delta" disabled/>
+            <NumberInput source="spectrum" disabled/>
+            <NumberInput source="total_Ar40" disabled/>
+            <NumberInput source="moles_of_K40" disabled/>
+            <NumberInput source="weight" disabled/>
+            <TextInput source="updatedby" disabled/>
+            <TextInput source="createdby" disabled/>
+            <TextInput source="lastupdate" disabled/>
+            <TextInput source="createdate" disabled/>
         </SimpleForm>
     </Edit>
 );
