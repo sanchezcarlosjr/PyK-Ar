878f1a5d844a3572d3309b4c1f297024673b9fbc
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-01-27 19:55:04 -0800

    feat(): show correct fields

--- web/src/app/potassium-argon-age-calculation/PotassiumArgonAgeCalculationList.tsx
+++ web/src/app/potassium-argon-age-calculation/PotassiumArgonAgeCalculationList.tsx
@@ -1,4 +1,4 @@
-import {Datagrid, DeleteButton, List, TextField, TextInput, BooleanInput} from "react-admin";
+import {Datagrid, DeleteButton, DateField, List, TextField, TextInput, BooleanInput} from "react-admin";
 import * as React from "react";
 
 const PotassiumArgonAgeCalculationFilters = [
@@ -12,7 +12,7 @@ export const PotassiumArgonAgeCalculationList = (props: any) => (
         <Datagrid rowClick="edit">
             <TextField source="age" label="Age (years)"/>
             <TextField source="spectrum_user_name" label="Spectrum user name" />
-            <TextField source="analysis_date" label="Analysis Date"/>
+            <DateField source="analysis_date" label="Analysis Date"/>
             <TextField source="weight" label="Weight (grams)"/>
             <TextField source="delta" label="Delta"/>
             <TextField source="gramsOfK" label="K (grams)"/>
--- web/src/app/potassium-argon-age-calculation/PotassiumArgonCalculationsEdit.tsx
+++ web/src/app/potassium-argon-age-calculation/PotassiumArgonCalculationsEdit.tsx
@@ -1,12 +1,17 @@
-import {DateInput, Edit, NumberInput, SimpleForm, TextInput} from "react-admin";
+import {DateInput, Edit, NumberInput, SimpleForm, TextInput, DateTimeInput} from "react-admin";
 import * as React from "react";
 
 export const PotassiumArgonCalculationsEdit = (props: any) => (
     <Edit {...props}>
         <SimpleForm>
-            <TextInput source="id"/>
-            <DateInput source="analysis_date" label="Analysis date"/>
-            <TextInput source="spectrum_user_name"/>
+            <TextInput source="id" fullWidth/>
+            <DateTimeInput source="analysis_date" label="Analysis date" fullWidth/>
+            <TextInput source="spectrum_user_name" fullWidth/>
+            <TextInput source="updatedby" disabled fullWidth label="Updated by"/>
+            <TextInput source="createdby" disabled fullWidth label="Created by"/>
+            <DateTimeInput source="lastupdate" disabled fullWidth label="Last update"/>
+            <DateTimeInput source="createdate" disabled fullWidth label="Create date"/>
+            <TextInput source="spectrum" disabled label="Spectrum" fullWidth/>
             <NumberInput source="age" disabled/>
             <NumberInput source="age_standard_deviation" disabled/>
             <NumberInput source="ar36_divides_ar38_t" disabled/>
@@ -21,14 +26,9 @@ export const PotassiumArgonCalculationsEdit = (props: any) => (
             <NumberInput source="moles_Ar38_in_tracer" disabled/>
             <NumberInput source="percentage_of_Ar40_rad_in_the_analysis" disabled/>
             <NumberInput source="delta" disabled/>
-            <NumberInput source="spectrum" disabled/>
             <NumberInput source="total_Ar40" disabled/>
             <NumberInput source="moles_of_K40" disabled/>
             <NumberInput source="weight" disabled/>
-            <TextInput source="updatedby" disabled/>
-            <TextInput source="createdby" disabled/>
-            <TextInput source="lastupdate" disabled/>
-            <TextInput source="createdate" disabled/>
         </SimpleForm>
     </Edit>
 );
