74d1cd5144cbaae3957d912cae4f1aabcefabbe4
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-01-27 21:25:58 -0800

    feat(ui): add filters

--- web/src/app/potassium-argon-age-calculation/PotassiumArgonAgeCalculationList.tsx
+++ web/src/app/potassium-argon-age-calculation/PotassiumArgonAgeCalculationList.tsx
@@ -1,21 +1,21 @@
-import {Datagrid, DeleteButton, DateField, List, TextField, TextInput, BooleanInput} from "react-admin";
+import {DateInput, Datagrid, DeleteButton, DateField, List, TextField, TextInput, BooleanInput} from "react-admin";
 import * as React from "react";
 
 const PotassiumArgonAgeCalculationFilters = [
     <TextInput source="id" label="Search by Sample ID" alwaysOn/>,
-    <TextInput source="spectrum_user_name" label="User" />,
+    <TextInput source="spectrum_user_name" label="Spectrum user name" />,
+    <DateInput source="analysis_date" label="Analysis Date" />,
     <BooleanInput source="deleted" label="Deleted" defaultValue={false}/>,
 ];
 
 export const PotassiumArgonAgeCalculationList = (props: any) => (
     <List {...props} filters={PotassiumArgonAgeCalculationFilters}>
         <Datagrid rowClick="edit">
+            <TextField source="id" label="Sample ID"/>
             <TextField source="age" label="Age (years)"/>
             <TextField source="spectrum_user_name" label="Spectrum user name" />
             <DateField source="analysis_date" label="Analysis Date"/>
             <TextField source="weight" label="Weight (grams)"/>
-            <TextField source="delta" label="Delta"/>
-            <TextField source="gramsOfK" label="K (grams)"/>
             <DeleteButton label=""/>
         </Datagrid>
     </List>
