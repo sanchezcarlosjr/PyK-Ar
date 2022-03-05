199e84ab5032a9efb2c374a7c063ed11d778455f
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-03-05 11:52:59 -0800

    refactor(test): it has got to create new test cases

--- apps/tests/test_measurement.py
+++ apps/tests/test_measurement.py
@@ -1,5 +1,5 @@
-import sys
 import json
+import sys
 from datetime import datetime
 
 from firebase_admin import initialize_app
@@ -9,25 +9,28 @@ from potassium_argon_age_calculation_mock_repository import PotassiumArgonAgeCal
 initialize_app()
 
 sys.path.append('../apps')
-from domain.experiments import Experiments
 from domain.measurement import Measurement
 from domain.T0 import T0
 from application.raw_mass_spectrometry_to_measurements_decorator import raw_mass_spectrometry_to_measurements
 from infrastructure.firestore_repository import FirestoreRepository
 
-with open('tests/example_request.json') as f:
-    sample = json.load(f)['data']
+
+def load_measurement_from_json(file: str) -> Measurement:
+    with open(file) as f:
+        sample = json.load(f)['data']
+    return raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
 
 
 def test_set_measurement_id():
-    measurement = Measurement(experiments=Experiments(sample['experiments']))
+    measurement = load_measurement_from_json("tests/example_request.json")
     mock = PotassiumArgonAgeCalculationMockRepository()
     m = mock.save(measurement)
     assert 0 <= float(m.id) <= 1, "Dict should set id"
 
 
 def test_filter_corrected_cycles():
-    e = Experiments(sample['experiments'])
+    measurement = load_measurement_from_json("tests/example_request.json")
+    e = measurement.experiments
     cycles = e.filter_corrected_cycles()
     for cycle in cycles:
         assert cycle.measure == "Corrected"
@@ -35,19 +38,23 @@ def test_filter_corrected_cycles():
 
 def test_equals_experiments():
     def test_raw_mass_spectrometry_to_measurements():
-        x = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
-        assert x.experiments == sample['experiments']
+        measurement = load_measurement_from_json("tests/example_request.json")
+        with open("tests/example_request.json") as f:
+            sample = json.load(f)['data']
+        assert measurement.experiments == sample['experiments']
 
     test_raw_mass_spectrometry_to_measurements()
 
 
 def test_convert_to_dict():
     def test_raw_mass_spectrometry_to_measurements():
-        x: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
-        measurement = x.to_dict()
+        measurement_obj = load_measurement_from_json("tests/example_request.json")
+        measurement = measurement_obj.to_dict()
         assert 'experiments' not in measurement
         assert 'blank_index' not in measurement
         assert 'sample_index' not in measurement
+        with open("tests/example_request.json") as f:
+            sample = json.load(f)['data']
         assert measurement['id'] == sample['experiments'][1]['sample_id']
         keys_to_check = [
             'spectrum_user_name',
@@ -64,21 +71,21 @@ def test_convert_to_dict():
 
 
 def test_calculate_moles_of_K40():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     assert measurement.moles_of_K40 == 0
     measurement.calculate_moles_of_K40()
     assert measurement.moles_of_K40 == 5.787E-8
 
 
 def test_calculate_moles_Ar38_in_tracer():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     assert measurement.moles_Ar38_in_tracer == 0
     measurement.calculate_moles_Ar38_in_tracer()
     assert measurement.moles_Ar38_in_tracer == 2.976E-10
 
 
 def test_calculate_Ar40_Ar38_ratio():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     assert measurement.Ar40_Ar38_ratio == 0
     measurement.calculate_Ar38()
     measurement.calculate_Ar40()
@@ -87,7 +94,7 @@ def test_calculate_Ar40_Ar38_ratio():
 
 
 def test_clone_experiment():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     old = measurement.experiments[0]
     new = measurement.experiments[0].filter_corrected_cycles()
     assert old != new
@@ -95,13 +102,13 @@ def test_clone_experiment():
 
 
 def test_should_calculate_cycles_mean():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     measurement.calculate_Ar36()
     assert measurement.Ar36 == 0.000114046680375
 
 
 def test_should_calculate_Ar40_Ar38_ratios_in_the_gas_mixture():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     measurement.Ar38 = 1
     measurement.Ar40 = 0.743
     measurement.calculate_Ar40_Ar38_ratio()
@@ -112,7 +119,7 @@ def test_should_calculate_Ar40_Ar38_ratios_in_the_gas_mixture():
 
 
 def test_should_calculate_Ar38_Ar36_ratios_in_the_gas_mixture():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     measurement.Ar38 = 1
     measurement.Ar36 = 0.98814229249
     measurement.calculate_Ar38_Ar36_ratio()
@@ -123,7 +130,7 @@ def test_should_calculate_Ar38_Ar36_ratios_in_the_gas_mixture():
 
 
 def test_should_calculate_total_Ar40():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     measurement.Ar38 = 1
     measurement.Ar40 = 0.743
     assert measurement.total_Ar40 == 0
@@ -132,7 +139,7 @@ def test_should_calculate_total_Ar40():
 
 
 def test_should_calculate_Ar40_rad():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     measurement.Ar36 = 0.98814229249
     measurement.Ar38 = 1
     measurement.Ar40 = 0.743
@@ -142,7 +149,7 @@ def test_should_calculate_Ar40_rad():
 
 
 def test_should_calculate_percentage_of_Ar40_rad_in_the_analysis():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     measurement.Ar36 = 0.98814229249
     measurement.Ar38 = 1
     measurement.Ar40 = 0.743
@@ -150,14 +157,16 @@ def test_should_calculate_percentage_of_Ar40_rad_in_the_analysis():
     measurement.calculate_percentage_of_Ar40_rad_in_the_analysis()
     assert measurement.percentage_of_Ar40_rad_in_the_analysis == 80.8
 
+
 def test_should_calculate_age():
-    measurement: Measurement = raw_mass_spectrometry_to_measurements(lambda m: m)(sample, {'user_id': 'A'})
+    measurement = load_measurement_from_json("tests/example_request.json")
     measurement.Ar36 = 0.98814229249
     measurement.Ar38 = 1
     measurement.Ar40 = 0.743
     assert measurement.age == 0
     measurement.calculate_age()
     assert measurement.age == 102603993.84
+
 
 def test_should_create_a_object_value_constant():
     t0_value = 3.086E-10
